6cfb8133655a745073b17e86d25efa1084952317
Marcosbito14 <marcoslbrito@hotmail.com>
2023-01-20 11:32:53 -0300

    Instalação do devise e i18n

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
+#Autenticação basica no browser
+#  http_basic_authenticate_with name: "marcos", password: "123"
+
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :profiles
   get 'welcome/index'
+  get 'inicio', to: 'welcome#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "welcome#index"
 end
